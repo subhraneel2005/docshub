57da7994a86be5594e421440cca24aa16bb477eb
subhraneel2005 <digitalsubhraneel@gmail.com>
2026-02-06 14:27:07 +0530

    feat: generate DocPlan + Repo summary + Single page contents using ai

--- apps/cli/src/commands/generate.ts
+++ apps/cli/src/commands/generate.ts
@@ -1,3 +1,5 @@
+import "dotenv/config"
+
 import prompts from "prompts";
 import chalk from "chalk";
 import { getRepoReadme } from "@repo/core/actions/github/get-repo-readme";
@@ -5,6 +7,9 @@ import { fetchRepoStructure } from "@repo/core/actions/github/fetch-file-tree";
 import { printRepoTree } from "../lib/print-repo-tree";
 import boxen from "boxen";
 import ora from "ora";
+import { contentGenerator } from "@repo/core/actions/ai/content-generator";
+import { repoSummariser } from "@repo/core/actions/ai/repo-summarizer"
+import { generatePages } from "@repo/core/actions/ai/generate-single-pages"
 
 export async function fetchRepoFlow(token: string) {
 
@@ -52,6 +57,40 @@ export async function fetchRepoFlow(token: string) {
         const structure = await fetchRepoStructure(token, owner, repo);
         spinner.succeed(chalk.hex("#5FCD01")("Folder structure fetched successfully!"));
         printRepoTree(structure);
+
+        const repoData = {
+            name: result.metadata.name?.toString() ?? "",
+            description: result.metadata.descriptions?.toString() ?? "",
+            language: result.metadata.language?.toString() ?? "",
+            topics: result.metadata.topics ?? [],
+            readme: result.readme?.toString() ?? "",
+            structure: printRepoTree(structure) ?? ""
+        };
+
+        console.log("\nrepodata:\n");
+        console.dir(repoData, { depth: null });
+
+        const spinner2 = ora({ text: chalk.hex("#5FCD01")("AI is generating docs content...") }).start();
+        const llmResponse = await contentGenerator(repoData);
+        spinner2.succeed(chalk.hex("#5FCD01")("Docs content generated successfully!"));
+
+        console.log("\nllm response:\n");
+        console.dir(llmResponse, { depth: null });
+
+        const spinner3 = ora({ text: chalk.hex("#5FCD01")("Generating AI summary...") }).start();
+        const repoSummary = await repoSummariser(repoData);
+        spinner3.succeed(chalk.hex("#5FCD01")("AI Summary generated successfully!"));
+
+        console.log("\nrepo summary:\n");
+        console.dir(repoSummary, { depth: null });
+
+        const spinner4 = ora({ text: chalk.hex("#5FCD01")("Generating single pages...") }).start();
+        const singlePages = await generatePages(repoSummary, llmResponse.pages);
+        spinner4.succeed(chalk.hex("#5FCD01")("Single pages generated successfully!"));
+
+        console.log("\nsingle pages:\n");
+        console.dir(singlePages, { depth: null });
+
     } catch (err: any) {
         console.log(chalk.red("failed to fetch repo"));
         console.error(err.message);
